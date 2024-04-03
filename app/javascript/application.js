91665d1201547df6bb6af8b1c1aa4c4d9fb7ad5c
hebiscus <akikopoland@gmail.com>
2024-04-03 13:21:52 +0200

    feat: add custom confirm dialog with tailwind ui

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+Turbo.setConfirmMethod(() => {
+    const dialog = document.getElementById("turbo-confirm")
+    dialog.showModal()
+
+    return new Promise((resolve,rejest) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue == "confirm")
+        }, {once: true})
+    })
+})
