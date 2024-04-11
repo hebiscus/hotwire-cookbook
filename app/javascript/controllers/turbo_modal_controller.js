83f576ca21738908dec8796d7480d18895666b19
hebiscus <akikopoland@gmail.com>
2024-04-11 14:13:31 +0200

    feat: wrap inside a dialog, add close button

---
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="turbo-modal"
+export default class extends Controller {
+  hideModal() {
+    this.element.parentElement.removeAttribute("src")
+    this.element.remove();
+    console.log("uhhh")
+  }
+}
